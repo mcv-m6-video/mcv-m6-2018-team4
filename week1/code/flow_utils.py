a303104955ae2870dbcac866208c6eb7cdcbedc1
Mikel Menta Garde <mkmenta95@gmail.com>
2018-02-28 02:19:35 +0100

    Deleted unuseful function

--- week1/code/flow_utils.py
+++ week1/code/flow_utils.py
@@ -34,30 +34,3 @@ def flow_error_map(F_gt, F_est):
     E[F_gt_val == 0] = 0
 
     return (E, F_gt_val)
-
-
-def flow_error_image(F_gt, F_est, tau, dilate_radius=1):
-    (E, F_val) = flow_error_map(F_gt, F_est)
-    F_mag = np.sqrt(F_gt[:, :, 0] ** 2 + F_gt[:, :, 1] ** 2)
-    aux = E / F_mag
-    aux[np.isnan(aux)] = np.inf
-    E_im = np.minimum(E / tau[0], aux / tau[1])
-    return E_im / np.max(E_im) * 255
-
-    ''' MATLAB Code
-    [E,F_val] = flow_error_map (F_gt,F_est);
-    F_mag = sqrt(F_gt(:,:,1).*F_gt(:,:,1)+F_gt(:,:,2).*F_gt(:,:,2));
-    E = min(E/tau(1),(E./F_mag)/tau(2));
-    
-    cols = error_colormap();
-    
-    F_err = zeros(size(F_gt));
-    for i=1:size(cols,1)
-      [v,u] = find(F_val > 0 & E >= cols(i,1) & E <= cols(i,2));
-      F_err(sub2ind(size(F_err),v,u,1*ones(length(v),1))) = cols(i,3);
-      F_err(sub2ind(size(F_err),v,u,2*ones(length(v),1))) = cols(i,4);
-      F_err(sub2ind(size(F_err),v,u,3*ones(length(v),1))) = cols(i,5);
-    end
-    
-    F_err = imdilate(F_err,strel('disk',dilate_radius));
-    '''

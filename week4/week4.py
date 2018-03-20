b1fd95fed8b9411108e528d8bf6949e8aa9f0649
mkmenta <mkmenta95@gmail.com>
2018-03-20 22:47:00 +0100

    Added grid search

--- week4/week4.py
+++ week4/week4.py
@@ -1,6 +1,7 @@
 import sys
 
 from lucas_kanade import optical_flow_lk
+import time
 
 from horn_schunk import optical_flow_hs
 
@@ -13,6 +14,7 @@ import os
 from block_matching import block_matching
 from dataset import Dataset
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def main():
@@ -33,13 +35,14 @@ def main():
     sequence.append(frame)
 
     # TASK 1.1 - Block Matching
-    # flow = block_matching(sequence[0], sequence[1], block_size=(3 * 8, 3 * 8), step=(8, 8), area=(8, 8))
+    # flow = block_matching(sequence[0], sequence[1], block_size=(16, 16), step=(8, 8), area=(24, 24), area_step=(1,1), error_thresh=1, verbose=True)
     #
     # flow_error(F_gt, flow)
     # rgb_flow = flow_visualization(flow[:, :, 0], flow[:, :, 1], 0)
     #
     # plt.imshow(rgb_flow)
     # plt.show()
+    # grid_search_block_matching(sequence, F_gt)
 
     # TASK 1.2 - Block Matching vs Other Techniques
     # sigma = 15;
@@ -55,10 +58,10 @@ def main():
     # TASK 2 - VIDEO STABILIZATION
 
     # Read traffic dataset
-    traffic_dataset = Dataset('traffic', 951, 1050)
+    # traffic_dataset = Dataset('traffic', 951, 1050)
 
-    traffic = traffic_dataset.readInput()
-    traffic_GT = traffic_dataset.readGT()
+    # traffic = traffic_dataset.readInput()
+    # traffic_GT = traffic_dataset.readGT()
 
     # Split dataset
     # traffic_train = traffic[:len(traffic)/2]
@@ -66,20 +69,57 @@ def main():
     # traffic_test_GT = traffic_GT[len(traffic)/2:]
 
     # TASK 2.1 - Video stabilization with Block Matching
-    seq_stab =  video_stabilization(traffic)
-    makeVideo(seq_stab, 'seq_stabilized.avi')
+    # video_stabilization(traffic)
+
     # TASK 2.2 - Block Matching Stabilization vs Other Techniques
 
     # TASK 2.3 - Stabilize your own video
 
-def makeVideo(sequence,video_name):
 
-    H, W, C, N = sequence.shape
-    video = cv2.VideoWriter(video_name, -1, 20.0, (W, H))
-    for i in range(N):
-        frame = sequence[:,:,:,i]
-        # Video writte
-        video.write(frame)
+def grid_search_block_matching(sequence, F_gt):
+    block_size_vector = np.arange(4, 21, step=2)
+    area_mult_vector = np.arange(1, 4, step=1)
+    mse_results = np.zeros([len(block_size_vector), len(area_mult_vector)])
+    pepn_results = np.zeros([len(block_size_vector), len(area_mult_vector)])
+
+    print('Grid size: {}*{}={}'.format(len(block_size_vector), len(area_mult_vector),
+                                       len(block_size_vector) * len(area_mult_vector)))
+    i = 0
+    for bs in block_size_vector:
+        j = 0
+        for am in area_mult_vector:
+            start = time.time()
+            F_est = block_matching(sequence[0], sequence[1], block_size=(bs, bs), step=(bs, bs),
+                                   area=(am * bs, am * bs))
+            MSE, PEPN = flow_error(F_gt, F_est)
+            print('Block Size: {} Area Multiplier: {} -> MSE: {} PEPN: {} Time: {}sec'.format(bs, am, MSE, PEPN,
+                                                                                              time.time() - start))
+            mse_results[i, j] = MSE
+            pepn_results[i, j] = PEPN
+            j += 1
+        i += 1
+    np.save('mse_results', mse_results)
+    np.save('pepn_results', pepn_results)
+    np.save('block_size_vector', block_size_vector)
+    np.save('area_mult_vector', area_mult_vector)
+    plot_grid_search(mse_results,block_size_vector,area_mult_vector,['Block size', 'Area multiplier', 'MSE'], save_to_file='MSE.png')
+
+
+def plot_grid_search(values, x_range, y_range, legend, save_to_file=None):
+    # Plot grid search
+    X, Y = np.meshgrid(x_range, y_range)
+    Z = values
+
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    ax.plot_surface(X, Y, Z, cmap='plasma')
+    ax.set_xlabel(legend[0])
+    ax.set_ylabel(legend[1])
+    ax.set_zlabel(legend[2])
+    if save_to_file is not None:
+        plt.savefig(save_to_file, dpi=300)
+    plt.show()
+
 
 if __name__ == "__main__":
     main()

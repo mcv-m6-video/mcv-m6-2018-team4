01e6f244b3146de6d345b7d134ce6717b65545b2
mkmenta <mkmenta95@gmail.com>
2018-04-11 15:37:35 +0200

    Working more or less

--- week5/objectTrackerCV2.py
+++ week5/objectTrackerCV2.py
@@ -46,41 +46,102 @@ def objectTrackerCV2(images, masks, distThreshold, verbose=False):
     nlbl = cc_mask.flatten().max()
     for lbl in range(1,nlbl+1):
         obj_coord = np.nonzero(cc_mask == lbl)
-        car_counter += 1
         object = detectedObject(car_counter, obj_coord, 0)
+        car_counter += 1
         bbox_cv2 = bbox_our_to_cv2(object.bbox) #(object.bbox[2],object.bbox[0],object.bbox[1]-object.bbox[0], object.bbox[3]-object.bbox[2])
         tracker = tracker_creator()
-        ok = tracker.init(mask, bbox_cv2) # Passing as input frame the current mask
+        ok = tracker.init(im_bb, bbox_cv2) # Passing as input frame the current mask
         trackers.append(tracker)
         detections.append(object) #add object to the detections list
         im_bb = drawBBox(im_bb, object.bbox, object.id) #draw the bounding box in the image
         mask = drawBBox(mask, object.bbox, object.id)
-    cv2.imshow('frame', mask)
-    cv2.waitKey(0)
+    #cv2.imshow('frame', mask)
+    #cv2.waitKey(0)
 
     frames_bb.append(im_bb)
+
+    #trackers = [trackers[3]]
+    #detections = [detections[3]]
 
     # Rest of frames
     for i in range(1,len(images)):
         im_bb = images[i].copy()
         mask = masks[i].copy().astype(np.uint8) * 255
         mask = np.tile(mask[:, :, np.newaxis], (1, 1, 3))
+        if i==34:
+            i
 
-        oklist=[]
-        for abc in range(len(trackers)):
-            if not detections[abc].finished:
-                ok, bbox = trackers[abc].update(mask)
+        # Get conected components
+        cc_mask = measure.label(mask, background=0)
+        nlbl = cc_mask.flatten().max()
+        ccobjects = []
+        for lbl in range(1, nlbl + 1):
+            # Create a temporal object to analize
+            obj_coord = np.nonzero(cc_mask == lbl)
+            object = detectedObject(np.nan, obj_coord, i)
+            ccobjects.append(object)
+
+        # Track detections
+        for j in range(len(trackers)):
+            if j==7:
+                j
+            if not detections[j].finished:
+                # Get tracked bbox
+                ok, bbox = trackers[j].update(im_bb)
+
+                # Check if the object still is visible
                 if not ok:
-                    detections[abc].finished=True
+                    detections[j].finished=True
                     continue
-                our_bbox = bbox_cv2_to_our(bbox)
-                detections[abc].updateBBox(our_bbox, i)
-                im_bb = drawBBox(im_bb, detections[abc].bbox, detections[abc].id)  # draw the bounding box in the image
-                mask = drawBBox(mask, detections[abc].bbox, detections[abc].id)
+                our_bbox = bbox_cv2_to_our(bbox) # convert bbox coords
 
-        sys.stdout.write('\n')
-        cv2.imshow('frame', mask)
-        cv2.waitKey(0)
+                # Find for the corresponding ccl
+                maxiou = -np.inf
+                for cc in ccobjects:
+                    iou= bbox_intersection_over_union(cc.bbox, our_bbox)
+                    if iou>maxiou:
+                        maxiou=iou
+                        best_match=cc
+
+                # Sometimes fails, so it needs an if
+                if len(ccobjects)==0:
+                    detection.finished=True
+                    continue
+                # Delete from ccl list
+                ccobjects.remove(best_match)
+
+
+                # Update detection with the cc bbox and reinit tracker
+                # (done because of the size and shape changes of the elements in the mask)
+                #mask = drawBBox(mask, best_match.bbox, detections[j].id)
+                detections[j].updateBBox(best_match.bbox, i)
+                if False:#maxiou < 1:
+                    trackers[j]=tracker_creator()
+                    trackers[j].init(mask,bbox_our_to_cv2(best_match.bbox))
+
+        # Check for new objects
+        for cc in ccobjects:
+            cc.id = car_counter
+            car_counter+=1
+            bbox_cv2 = bbox_our_to_cv2(cc.bbox)
+            tracker = tracker_creator()
+            ok = tracker.init(im_bb, bbox_cv2)  # Passing as input frame the current mask
+            trackers.append(tracker)
+            detections.append(cc)
+
+        # Draw bboxes
+        for detection in detections:
+            if not detection.finished:
+                im_bb = drawBBox(im_bb, detection.bbox, detection.id)  # draw the bounding box in the image
+                mask = drawBBox(mask, detection.bbox, detection.id)
+
+        sys.stdout.write('{}:'.format(i))
+        for detection in detections:
+            if not detection.finished:
+                sys.stdout.write('{},'.format(detection.id))
+        sys.stdout.write('  len:{}\n'.format(len(detections)))
+        #cv2.imshow('frame', mask)
+        #cv2.waitKey(0)
 
         """
         Para cada connected component:
@@ -88,71 +149,6 @@ def objectTrackerCV2(images, masks, distThreshold, verbose=False):
         2.- En tal caso hacer el update BBox (ahora si) basandonos en el bbox de cc
         3.- Hacemos init del algoritmo
         """
-        cc_mask = measure.label(mask, background=0)  # get conected components
-        nlbl = cc_mask.flatten().max()
-        for lbl in range(1, nlbl + 1):
-            # Create a temporal object to analize
-            obj_coord = np.nonzero(cc_mask == lbl)
-            object = detectedObject(np.nan, obj_coord, i)
-
-        """
-        for detection in detections:
-            # Make all the detections not visible
-            detection.visible = False
-
-            # Make all the detections not visible
-            if((i- detection.framesList[-1])>5 and not detection.finished):
-                detection.finished = True
-                if verbose: print "Object "+ str(detection.id)+ "deleted"
-
-        cc_mask = measure.label(mask, background=0) #get conected components
-        nlbl = cc_mask.flatten().max()
-        for lbl in range(1, nlbl + 1):
-            # Create a temporal object to analize
-            obj_coord = np.nonzero(cc_mask == lbl)
-            object = detectedObject(np.nan, obj_coord, i)
-
-            # Search variables inizalitation
-            minDist = np.inf
-            objectFound = False
-
-            print "Label "+ str(lbl)
-
-            for detection in detections:
-                if not detection.finished:
-                    # Predict position with Kalman
-                    prediction = detection.kalman.predict()
-                    dist = euclideanDistance(object.center, prediction)
-
-                    print "\t dist to " + str(detection.id)+ ": " +  str(dist)
-                    # Search the nearest object
-                    if(dist < distThreshold):
-                        if dist < minDist:
-                            minDist = dist
-                            nearest_detection = detection
-                            objectFound = True
-
-            if objectFound:
-                # Update Kalman
-                nearest_detection.kalman.update(object.center)
-
-                # Make detection visible and update the Bounding Box
-                nearest_detection.visible = True
-                nearest_detection.updateBBox(object.bbox, i)
-                print "\n\t assigned to " + str(nearest_detection.id)
-            else:
-                # Add the new detected object to the detections list
-                car_counter +=1
-                object.id = car_counter
-                detections.append(object)
-                print "\n\t Created object " + str(nearest_detection.id)
-        """
-
-        # Draw the bounding boxes for the visible detections
-        for detection in detections:
-            if not detection.finished:
-                im_bb = drawBBox(im_bb, detection.bbox, detection.id, center=(int(detection.center[1]),int(detection.center[0])), kalman=(int(detection.kalman.priorEstimateY), int(detection.kalman.priorEstimateX)))
-
         # cv2.imshow('frame', im_bb)
         # cv2.waitKey(0)
 
@@ -162,6 +158,42 @@ def objectTrackerCV2(images, masks, distThreshold, verbose=False):
     sys.stdout.write(str(elapsed) + ' sec \n')
     print "Cars detected " + str(car_counter)
     return frames_bb, detections
+
+
+def euclideanDistance(point1, point2):
+    dist = np.sqrt((point1[0]-point2[0])**2 + (point1[1]-point2[1])**2)
+    return dist
+
+def bbox_cv2_to_our(bbox):
+    return (int(bbox[1]), int(bbox[2] + bbox[1]-1), int(bbox[0]), int(bbox[3] + bbox[0]-1))
+
+def bbox_our_to_cv2(bbox):
+    return (int(bbox[2]), int(bbox[0]), int(bbox[1] - bbox[0]+1), int(bbox[3] - bbox[2]+1))
+
+
+def bbox_intersection_over_union(boxA, boxB):
+    # Done for topleft bottright bbox with 0,0 in topleft corner of image
+    # determine the (x, y)-coordinates of the intersection rectangle
+    xA = max(boxA[0], boxB[0])
+    yA = max(boxA[1], boxB[1])
+    xB = min(boxA[2], boxB[2])
+    yB = min(boxA[3], boxB[3])
+
+    # compute the area of intersection rectangle
+    interArea = (xB - xA + 1) * (yB - yA + 1)
+
+    # compute the area of both the prediction and ground-truth
+    # rectangles
+    boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
+    boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
+
+    # compute the intersection over union by taking the intersection
+    # area and dividing it by the sum of prediction + ground-truth
+    # areas - the interesection area
+    iou = interArea / float(boxAArea + boxBArea - interArea)
+
+    # return the intersection over union value
+    return iou
 
 def drawBBox(im, bbox, id, vel=None, center=None, kalman=None):
     # bounding box
@@ -220,39 +252,3 @@ def drawBBox(im, bbox, id, vel=None, center=None, kalman=None):
         im = cv2.circle(im, kalman, 4, (0, 0, 255), -1)
 
     return im
-
-
-def euclideanDistance(point1, point2):
-    dist = np.sqrt((point1[0]-point2[0])**2 + (point1[1]-point2[1])**2)
-    return dist
-
-def bbox_cv2_to_our(bbox):
-    return (int(bbox[1]), int(bbox[2] + bbox[1]), int(bbox[0]), int(bbox[3] + bbox[0]))
-
-def bbox_our_to_cv2(bbox):
-    return (int(bbox[2]), int(bbox[0]), int(bbox[1] - bbox[0]), int(bbox[3] - bbox[2]))
-
-
-def bb_intersection_over_union(boxA, boxB):
-    # Done for topleft bottright
-    # determine the (x, y)-coordinates of the intersection rectangle
-    xA = max(boxA[0], boxB[0])
-    yA = max(boxA[1], boxB[1])
-    xB = min(boxA[2], boxB[2])
-    yB = min(boxA[3], boxB[3])
-
-    # compute the area of intersection rectangle
-    interArea = (xB - xA + 1) * (yB - yA + 1)
-
-    # compute the area of both the prediction and ground-truth
-    # rectangles
-    boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
-    boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
-
-    # compute the intersection over union by taking the intersection
-    # area and dividing it by the sum of prediction + ground-truth
-    # areas - the interesection area
-    iou = interArea / float(boxAArea + boxBArea - interArea)
-
-    # return the intersection over union value
-    return iou

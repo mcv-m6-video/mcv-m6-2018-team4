14b4ee158684981d486fb1553255ef1c7d84678f
mkmenta <mkmenta95@gmail.com>
2018-04-11 02:54:27 +0200

    Design of the 2nd part

--- week5/objectTrackerCV2.py
+++ week5/objectTrackerCV2.py
@@ -48,7 +48,7 @@ def objectTrackerCV2(images, masks, distThreshold, verbose=False):
         obj_coord = np.nonzero(cc_mask == lbl)
         car_counter += 1
         object = detectedObject(car_counter, obj_coord, 0)
-        bbox_cv2 = (object.bbox[2],object.bbox[0],object.bbox[1]-object.bbox[0], object.bbox[3]-object.bbox[2])
+        bbox_cv2 = bbox_our_to_cv2(object.bbox) #(object.bbox[2],object.bbox[0],object.bbox[1]-object.bbox[0], object.bbox[3]-object.bbox[2])
         tracker = tracker_creator()
         ok = tracker.init(mask, bbox_cv2) # Passing as input frame the current mask
         trackers.append(tracker)
@@ -68,19 +68,33 @@ def objectTrackerCV2(images, masks, distThreshold, verbose=False):
 
         oklist=[]
         for abc in range(len(trackers)):
-            ok, bbox = trackers[abc].update(mask)
-            if not ok:
-                detection.finished=True
-            oklist.append(ok)
-            our_bbox = np.array([bbox[1],bbox[2]+bbox[1],bbox[0],bbox[3]+bbox[0]],dtype=np.int)
-            detections[abc].updateBBox(our_bbox, i)
-            im_bb = drawBBox(im_bb, detections[abc].bbox, detections[abc].id)  # draw the bounding box in the image
-            mask = drawBBox(mask, detections[abc].bbox, detections[abc].id)
-        for ok in oklist:
-            sys.stdout.write('{}, '.format(ok))
+            if not detections[abc].finished:
+                ok, bbox = trackers[abc].update(mask)
+                if not ok:
+                    detections[abc].finished=True
+                    continue
+                our_bbox = bbox_cv2_to_our(bbox)
+                detections[abc].updateBBox(our_bbox, i)
+                im_bb = drawBBox(im_bb, detections[abc].bbox, detections[abc].id)  # draw the bounding box in the image
+                mask = drawBBox(mask, detections[abc].bbox, detections[abc].id)
+
         sys.stdout.write('\n')
         cv2.imshow('frame', mask)
         cv2.waitKey(0)
+
+        """
+        Para cada connected component:
+        1.- Comprobar IOU para ver si es el mismo
+        2.- En tal caso hacer el update BBox (ahora si) basandonos en el bbox de cc
+        3.- Hacemos init del algoritmo
+        """
+        cc_mask = measure.label(mask, background=0)  # get conected components
+        nlbl = cc_mask.flatten().max()
+        for lbl in range(1, nlbl + 1):
+            # Create a temporal object to analize
+            obj_coord = np.nonzero(cc_mask == lbl)
+            object = detectedObject(np.nan, obj_coord, i)
+
         """
         for detection in detections:
             # Make all the detections not visible
@@ -211,3 +225,34 @@ def drawBBox(im, bbox, id, vel=None, center=None, kalman=None):
 def euclideanDistance(point1, point2):
     dist = np.sqrt((point1[0]-point2[0])**2 + (point1[1]-point2[1])**2)
     return dist
+
+def bbox_cv2_to_our(bbox):
+    return (int(bbox[1]), int(bbox[2] + bbox[1]), int(bbox[0]), int(bbox[3] + bbox[0]))
+
+def bbox_our_to_cv2(bbox):
+    return (int(bbox[2]), int(bbox[0]), int(bbox[1] - bbox[0]), int(bbox[3] - bbox[2]))
+
+
+def bb_intersection_over_union(boxA, boxB):
+    # Done for topleft bottright
+    # determine the (x, y)-coordinates of the intersection rectangle
+    xA = max(boxA[0], boxB[0])
+    yA = max(boxA[1], boxB[1])
+    xB = min(boxA[2], boxB[2])
+    yB = min(boxA[3], boxB[3])
+
+    # compute the area of intersection rectangle
+    interArea = (xB - xA + 1) * (yB - yA + 1)
+
+    # compute the area of both the prediction and ground-truth
+    # rectangles
+    boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
+    boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
+
+    # compute the intersection over union by taking the intersection
+    # area and dividing it by the sum of prediction + ground-truth
+    # areas - the interesection area
+    iou = interArea / float(boxAArea + boxBArea - interArea)
+
+    # return the intersection over union value
+    return iou
